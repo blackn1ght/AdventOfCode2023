47cf22e06831328171b8812746ba55cb6e36ec7c
Tom Cowell <tom.blacknight@gmail.com>
2023-12-01 17:28:28 +0000

    chore: upgraded to dotnet 8

--- Utils/ChallengeBase.cs
+++ Utils/ChallengeBase.cs
@@ -10,14 +10,12 @@ public abstract class ChallengeBase<T>
     }
 
     public T GetAnswerForPart(ChallengePart part)
-    {
-        return part switch
+        => part switch
         {
             ChallengePart.Part1 => Part1(),
             ChallengePart.Part2 => Part2(),
             _ => throw new ArgumentException()
         };
-    }
 
     protected abstract T Part1();
     protected abstract T Part2();
--- Utils/ChallengeDataReader.cs
+++ Utils/ChallengeDataReader.cs
@@ -3,14 +3,13 @@ namespace AdventOfCode2023.Utils;
 public static class ChallengeDataReader
 {
     public static string[] GetDataForDay(int day, InputTypes inputType)
-    {
-        return inputType switch 
+        => inputType switch 
         {
             InputTypes.Input => ReadFile(day, "input.txt"),
             InputTypes.Example => ReadFile(day, "example.txt"),
             _ => throw new ArgumentException($"Invalid inputType {Enum.GetName(inputType)}")
         };
-    }
+    
 
     private static string[] ReadFile(int day, string filename)
     {
