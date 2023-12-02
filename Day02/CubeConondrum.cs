e37b46e33dd582b78d9c20a76a75dff4f4c9f2eb
Tom Cowell <tom.blacknight@gmail.com>
2023-12-02 12:13:41 +0000

    refactor: day02

--- Day02/CubeConondrum.cs
+++ Day02/CubeConondrum.cs
@@ -4,66 +4,34 @@ using System.Text.RegularExpressions;
 
 public class CubeConondrum : ChallengeBase<int>
 {
+    private readonly Dictionary<string, int> _colours = new ()
+    {
+        { "red", 12 },
+        { "green", 13 },
+        { "blue", 14 }
+    };
 
-    // 12 red cubes, 13 green cubes, and 14 blue cubes
     public CubeConondrum(string[] data) : base(data)
     {
     }
 
     protected override int Part1()
-    {
-        var answer = 0;
-
-        var thresholds = new Dictionary<string, int>()
-        {
-            { "red", 12 },
-            { "green", 13 },
-            { "blue", 14 }
-        };
-
-        var counter = 1;
-        foreach (var row in ChallengeDataRows)
-        {
-            var thresholdsHit = false;
-            foreach (var threshold in thresholds)
-            {
-                var matches = Regex.Matches(row, $"([0-9]*) {threshold.Key}");
-                if (matches.Any(m => int.Parse(m.Groups[1].Value) > threshold.Value))
-                {
-                    thresholdsHit = true;
-                }
-            }
-            if (thresholdsHit == false)
-            {
-                answer += counter;
-            }
-            counter++;
-        }
-
-        return answer;
-    }
-
+        => ChallengeDataRows
+            .Select((row, index) => _colours
+                .Select(colour => ValuesForColour(row, colour.Key).Any(m => m > colour.Value))
+                .All(x => !x) ? index + 1 : 0)
+            .Sum();
+    
     protected override int Part2()
-    {
-        var answer = 0;
+        => ChallengeDataRows
+            .Sum(row => _colours
+                .Select(colour => ValuesForColour(row, colour.Key).Max())
+                .Aggregate(1, (prev, curr) => prev * curr)
+            );
+     
 
-        var colours = new[] { "blue", "red", "green"};
-
-        foreach (var row in ChallengeDataRows)
-        {
-            var colourResults = new List<int>();
-
-            foreach (var colour in colours)
-            {
-                var matches = Regex.Matches(row, $"([0-9]*) {colour}");
-                var max = matches.Max(m => int.Parse(m.Groups[1].Value));
-
-                colourResults.Add(max);
-            }
-
-            answer += colourResults.Aggregate(1, (prev, curr) => prev * curr);
-        }
-
-        return answer;
-    }   
+    private static IEnumerable<int> ValuesForColour(string input, string colour) 
+        => Regex
+            .Matches(input, $"([0-9]*) {colour}")
+            .Select(m => int.Parse(m.Groups[1].Value));
 }
